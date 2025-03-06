660ef270bff4e14725fd2d2bd39fdf19c7e442aa
femaflam22 <femaflam22@gmail.com>
2025-03-06 12:18:10 +0700

    cors

--- routes/web.php
+++ routes/web.php
@@ -16,43 +16,45 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+$router->group(['middleware' => 'cors'], function () use ($router) {
 
-$router->post('/login', 'UserController@login');
+    $router->post('/login', 'UserController@login');
 
-$router->group(['middleware' => 'auth'], function () use ($router) {
-    $router->get('/me', 'UserController@me');
-    $router->get('/logout', 'UserController@logout');
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->get('/me', 'UserController@me');
+        $router->get('/logout', 'UserController@logout');
 
-    $router->post('/register', 'UserController@register');
-    $router->group(['prefix' => 'users'], function () use ($router) {
-        $router->get('/', 'UserController@index');
-        $router->patch('/non-aktif/{userId}', 'UserController@nonAktif');
-    });
+        $router->post('/register', 'UserController@register');
+        $router->group(['prefix' => 'users'], function () use ($router) {
+            $router->get('/', 'UserController@index');
+            $router->patch('/non-aktif/{userId}', 'UserController@nonAktif');
+        });
 
-    $router->group(['prefix' => 'stuffs'], function () use ($router) {
-        $router->get('/', 'StuffController@index');
-        $router->post('/', 'StuffController@store');
-        $router->get('/trash', 'StuffController@trash');
-        $router->get('/{id}', 'StuffController@show');
-        $router->patch('/{id}', 'StuffController@update');
-        $router->delete('/{id}', 'StuffController@destroy');
-        $router->get('/trash/restore/{id}', 'StuffController@restore');
-        $router->delete('/trash/delete/{id}', 'StuffController@permanentDelete');
-    });
+        $router->group(['prefix' => 'stuffs'], function () use ($router) {
+            $router->get('/', 'StuffController@index');
+            $router->post('/', 'StuffController@store');
+            $router->get('/trash', 'StuffController@trash');
+            $router->get('/{id}', 'StuffController@show');
+            $router->patch('/{id}', 'StuffController@update');
+            $router->delete('/{id}', 'StuffController@destroy');
+            $router->get('/trash/restore/{id}', 'StuffController@restore');
+            $router->delete('/trash/delete/{id}', 'StuffController@permanentDelete');
+        });
 
-    $router->group(['prefix' => 'inbound-stuffs'], function () use ($router) {
-        $router->get('/', 'InboundStuffController@index');
-        $router->post('/', 'InboundStuffController@store');
-        $router->delete('/{id}', 'InboundStuffController@destroy');
-    });
+        $router->group(['prefix' => 'inbound-stuffs'], function () use ($router) {
+            $router->get('/', 'InboundStuffController@index');
+            $router->post('/', 'InboundStuffController@store');
+            $router->delete('/{id}', 'InboundStuffController@destroy');
+        });
 
-    $router->group(['prefix' => 'lendings'], function () use ($router) {
-        $router->get('/', 'LendingController@index');
-        $router->post('/', 'LendingController@store');
-    });
+        $router->group(['prefix' => 'lendings'], function () use ($router) {
+            $router->get('/', 'LendingController@index');
+            $router->post('/', 'LendingController@store');
+        });
 
-    $router->group(['prefix' => 'restorations'], function () use ($router) {
-        $router->get('/', 'RestorationController@index');
-        $router->post('/', 'RestorationController@store');
+        $router->group(['prefix' => 'restorations'], function () use ($router) {
+            $router->get('/', 'RestorationController@index');
+            $router->post('/', 'RestorationController@store');
+        });
     });
 });
